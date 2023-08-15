9106165e5076966f24a091f3fac0093fa3e3ea4d
Luiz <lucachaves@gmail.com>
2023-08-15 07:12:27 -0300

    ajuste no iframe

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -15,6 +15,59 @@ export default function Iframe({ src, srcDoc, height }: Props) {
 
   const $isDark = useStore(isDark);
 
+  iFrameRef?.current?.contentWindow?.document.head.insertAdjacentHTML(
+    'beforeend',
+    `<style>
+      :root {
+        --color-primary: #000;
+      }
+
+      :root.dark {
+        --color-primary: #fff;
+      }
+
+      body {
+        color: var(--color-primary);
+      }
+
+      a {
+        color: var(--color-primary);
+      }
+
+      * {
+        border-color: var(--color-primary);
+      }
+
+      /* * {
+         border-color: var(--color-primary) !important;
+       } */
+
+       ::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+      }
+
+      ::-webkit-scrollbar-button:start:decrement,
+      ::-webkit-scrollbar-button:end:increment {
+        display: none;
+      }
+
+      *::-webkit-scrollbar-track-piece {
+        @apply bg-transparent;
+        -webkit-border-radius: 6px;
+      }
+
+      *::-webkit-scrollbar-thumb:vertical {
+        @apply bg-gray-400;
+        -webkit-border-radius: 6px;
+      }
+
+      * {
+        scrollbar-color: #9ca3af transparent;
+      }
+    </style>`
+  );
+
   const handleResize = useCallback(
     (iframe: React.RefObject<HTMLIFrameElement>) => {
       const { body, documentElement } =
@@ -36,59 +89,6 @@ export default function Iframe({ src, srcDoc, height }: Props) {
 
   useEffect(() => {
     handleResize(iFrameRef);
-
-    iFrameRef?.current?.contentWindow?.document.head.insertAdjacentHTML(
-      'beforeend',
-      `<style>
-        :root {
-          --color-primary: #000;
-        }
-
-        :root.dark {
-          --color-primary: #fff;
-        }
-
-        body {
-          color: var(--color-primary);
-        }
-
-        a {
-          color: var(--color-primary);
-        }
-
-        * {
-          border-color: var(--color-primary);
-        }
-
-        /* * {
-           border-color: var(--color-primary) !important;
-         } */
-
-         ::-webkit-scrollbar {
-          width: 10px;
-          height: 10px;
-        }
-
-        ::-webkit-scrollbar-button:start:decrement,
-        ::-webkit-scrollbar-button:end:increment {
-          display: none;
-        }
-
-        *::-webkit-scrollbar-track-piece {
-          @apply bg-transparent;
-          -webkit-border-radius: 6px;
-        }
-
-        *::-webkit-scrollbar-thumb:vertical {
-          @apply bg-gray-400;
-          -webkit-border-radius: 6px;
-        }
-
-        * {
-          scrollbar-color: #9ca3af transparent;
-        }
-      </style>`
-    );
   }, [handleResize, iFrameRef]);
 
   useEffect(() => {
