8a367b90c1759a7fc6bb10a9236c0a0106922f20
Luiz <lucachaves@gmail.com>
2024-03-10 23:39:23 -0300

    dados da disciplina

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,36 @@
+// subject data
+export const SUBJECT = {
+  CLASSROOM_URLS: {
+    A: 'https://classroom.google.com/c/NjYzNjMxMjQyMTQz',
+    B: 'https://classroom.google.com/c/NjY0NDQwNDk2MTcz',
+  },
+  CHAT_URL: 'https://mail.google.com/mail/u/0/#chat/space/AAAAujjJnC0',
+  PROJECTS_URL: 'https://padlet.com/lucachaves/projetolm20241',
+  GITHUB_URLS: {
+    A: 'https://github.com/lucachaves/lm-2024-1',
+  },
+  SCHEDULES: [
+    {
+      className: 'A',
+      room: 'Lab 58',
+      day: 'Segunda',
+      start: '13:00',
+      end: '14:40',
+    },
+    {
+      className: 'B',
+      room: 'Lab 4',
+      day: 'Segunda',
+      start: '13:00',
+      end: '14:40',
+    },
+  ],
+};
+
+// project data
 export const GITHUB_EDIT_URL = 'https://github.com/ifpb/lm/tree/main';
 
-export const CLASSNOTE_SLUGS = [
+export const CONTENT_SLUGS = [
   // 'web-server/live-server',
   'markup/markup-language',
   'html/introduction-html',
